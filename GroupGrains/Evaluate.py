702402dc83ce5107472cb4d8cc6e1edede6526ed
Zhang Yiyi <zyy991018@buaa.edu.cn>
2025-08-14 23:47:17 +0800

    update GroupGrains res_ep1

--- GroupGrains/Evaluate.py
+++ GroupGrains/Evaluate.py
@@ -53,7 +53,9 @@ def evaluate_model(model, xyz, idx, scaling):
     invC = torch.inverse(C) # C的逆
     
     # 6. 计算Green-Lagrange应变 E = 0.5*(C - I)
-    E = 0.5 * (C - I)
+    epsilon = 0.5 * (C - I)
+    principal_strains = torch.linalg.eigvalsh(epsilon)     # 形状 [n, 3]，升序排列
+    e1 = principal_strains[:, -1]
 
     # 7. 计算第二类Piola-Kirchhoff应力S = mu*(I - C^(-1)) + λ*ln(J)*C^(-1)
     S = mu * (I - invC) + lam * torch.log(J).unsqueeze(-1).unsqueeze(-1) * invC
@@ -80,12 +82,12 @@ def evaluate_model(model, xyz, idx, scaling):
     u = u_pred.detach().cpu().numpy()/scaling
     U = (np.float64(u[:,0]), np.float64(u[:,1]), np.float64(u[:,2]))
     # Green-Lagrange应变张量分量
-    E11 = E[:, 0, 0].detach().cpu().numpy()
-    E12 = E[:, 0, 1].detach().cpu().numpy()
-    E13 = E[:, 0, 2].detach().cpu().numpy()
-    E22 = E[:, 1, 1].detach().cpu().numpy()
-    E23 = E[:, 1, 2].detach().cpu().numpy()
-    E33 = E[:, 2, 2].detach().cpu().numpy()
+    E11 = epsilon[:, 0, 0].detach().cpu().numpy()
+    E12 = epsilon[:, 0, 1].detach().cpu().numpy()
+    E13 = epsilon[:, 0, 2].detach().cpu().numpy()
+    E22 = epsilon[:, 1, 1].detach().cpu().numpy()
+    E23 = epsilon[:, 1, 2].detach().cpu().numpy()
+    E33 = epsilon[:, 2, 2].detach().cpu().numpy()
     # 第二类Piola-Kirchhoff应力张量分量
     S11 = S[:, 0, 0].detach().cpu().numpy()
     S12 = S[:, 0, 1].detach().cpu().numpy()
@@ -102,9 +104,10 @@ def evaluate_model(model, xyz, idx, scaling):
     sigma33=s33.detach().cpu().numpy()
     # Von Mises应力    
     SVonMises = SVonMises.detach().cpu().numpy()
+    e1 = e1.detach().cpu().numpy()
     
 
-    return U, SVonMises, \
+    return U, SVonMises, e1, \
            E11, E12, E13, E22, E23, E33, \
            S11, S12, S13, S22, S23, S33, \
            sigma11, sigma12, sigma13, sigma22, sigma23, sigma33
@@ -124,14 +127,14 @@ for data_idx in range(cfg.data_num):
     # 计算该有限元网格对应的预测值
     xyz=mesh.points
 
-    U, SVonMises, \
+    U, SVonMises, e1,\
     E11, E12, E13, E22, E23, E33, \
     S11, S12, S13, S22, S23, S33, \
     sigma11, sigma12, sigma13, sigma22, sigma23, sigma33= evaluate_model(model, xyz, data_idx, cfg.model_scale)
 
     # all_cells=mesh.cell_data['gmsh:physical'][6]
     # 写入vtu网格文件
-    util.FEMmeshtoVTK(f"{cfg.Evaluate_save_path}/{data_idx}_epoch{dem_epoch}", mesh, pointdata={"U": U, "SVonMises": SVonMises, \
+    util.FEMmeshtoVTK(f"{cfg.Evaluate_save_path}/{data_idx}_epoch{dem_epoch}", mesh, pointdata={"U": U, "SVonMises": SVonMises, "e1":e1,\
                                                                                  "E11": E11, "E12": E12, "E13": E13, \
                                                                                  "E22": E22, "E23": E23, "E33": E33, \
                                                                                  "S11": S11, "S12": S12, "S13": S13, \
@@ -142,7 +145,9 @@ for data_idx in range(cfg.data_num):
 
     U_data=np.hstack((xyz, np.linalg.norm(np.array(U).T, axis=1).reshape(-1,1)))
     SVonMises_data=np.hstack((xyz, SVonMises.reshape(-1,1)))
+    e1_data=np.hstack((xyz, e1.reshape(-1,1)))
 
     np.savetxt(f"{cfg.model_save_path}/{data_idx}_U_epoch{dem_epoch}.txt", U_data, fmt='%f', delimiter=' ')
     np.savetxt(f"{cfg.model_save_path}/{data_idx}_SVonMises_epoch{dem_epoch}.txt", SVonMises_data, fmt='%f', delimiter=' ')
+    np.savetxt(f"{cfg.model_save_path}/e1_epoch{dem_epoch}.txt", e1_data, fmt='%f', delimiter=' ')
     print(f"模型dem_epoch{dem_epoch}.pth的预测结果已保存到{cfg.model_save_path}/{data_idx}_U_epoch{dem_epoch}.txt")
